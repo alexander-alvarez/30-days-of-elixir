6c46f60aaee2d0dca8610395723a7382c1733279
Alex Alvarez <dominalexican@gmail.com>
2017-01-02 18:31:36 -0500

    Update 03-input-output.exs

--- 03-input-output.exs
+++ 03-input-output.exs
@@ -1,60 +1,15 @@
-# http://elixir-lang.org/docs/stable/elixir/IO.html
-# http://elixir-lang.org/docs/stable/elixir/File.html
-
-defmodule CowInterrogator do
-
-  # docstring are quite useful you can generate docs out of them
-  # check: http://elixir-lang.org/getting-started/module-attributes.html
-  # for more info
-  @doc """
-  Gets name from standard IO
-  """
-  def get_name do
-    IO.gets("What is your name? ")
-    |> String.strip
-  end
-
-  def get_cow_lover do
-    IO.getn("Do you like cows? [y|n] ", 1)
-  end
-
-  def interrogate do
-    name = get_name
-    case String.downcase(get_cow_lover) do
-      "y" ->
-        IO.puts "Great! Here's a cow for you #{name}:"
-        IO.puts cow_art
-      "n" ->
-        IO.puts "That's a shame, #{name}."
-      _ ->
-        IO.puts "You should have entered 'y' or 'n'."
-    end
-  end
-
-  def cow_art do
-    path = Path.expand("support/cow.txt", __DIR__)
-    case File.read(path) do
-      {:ok, art} -> art
-      {:error, _} -> IO.puts "Error: cow.txt file not found"; System.halt(1)
-    end
-  end
-end
-
-ExUnit.start
-
-
-defmodule InputOutputTest do
-  use ExUnit.Case
-  import String
-
-  test "checks if cow_art returns string from support/cow.txt" do
-    # this call checks if cow_art function returns art from txt file
-    art = CowInterrogator.cow_art
-    assert strip(art) |> first == "(" # first is implemented in String module
-  end
-end
-
-# call interrogate performs most of the work
-# asks about your name, your interests in cows
-# and renders cow art
-CowInterrogator.interrogate
+# TASK: Create an interactive program that reads from STDIN.
+# - Ask the user: "What is your name?"
+# - Ask the user: "Do you like cows? [y|n]"
+#   - If the user responds 'y' or 'Y', read the cow from `cow.txt` and print it to the console.
+#       -  In an error occurs in reading the file, print "Error: cow.txt file not found" to the console.
+#   - If the user responds 'n' or 'N', print "That's a shame" followed by the user's name to the console.
+#   - Any other input, print "You should have entered 'y' or 'n'." to the console.
+#
+# BONUS:
+#   - Write a test case leveraging module you wrote to that asserts the first non-whitespace character of the cow
+#      ASCII art is '('
+#
+# resources: 
+#   - support/cow.txt
+#
